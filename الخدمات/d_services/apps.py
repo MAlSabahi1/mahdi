73877bca55921eaf1cdf72d0139eee6814895a97
Mahdi Abu Ali <mhdy00637@gmail.com>
2026-07-07 16:38:00 +0300

    delet servics

--- الخدمات/d_services/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DServicesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'd_services'
-    verbose_name = 'الخدمات الديناميكية'
--- الخدمات/d_services/models/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-import importlib
-
-# استيراد جميع الملفات داخل المجلد الحالي
-for module in os.listdir(os.path.dirname(__file__)):
-    if module.endswith('.py') and module != '__init__.py':
-        importlib.import_module(f'{__name__}.{module[:-3]}')
