3a640a1b132ed3a8cb398e1a0934f27522a9c027
Ibrahim <your.email@example.com>
2026-07-11 22:43:25 +0300

    44444444444444444

--- Backend/fix_db.py
+++
@@ -1,8 +0,0 @@
-import os
-import django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
-django.setup()
-from django.db import connection
-with connection.cursor() as cursor:
-    cursor.execute("ALTER TABLE authorization_role DROP COLUMN IF EXISTS permissions;")
-print("Dropped permissions column")
